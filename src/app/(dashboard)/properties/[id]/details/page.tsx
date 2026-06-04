2afd6352383f2403616ebd4f55e0c5af6e91b994
Masterkimaru <themasterskimaru@gmail.com>
2026-06-05 00:23:01 +0300

    changed Payment Report to Property Statement

--- src/app/(dashboard)/properties/[id]/details/page.tsx
+++ src/app/(dashboard)/properties/[id]/details/page.tsx
@@ -1070,7 +1070,7 @@ export default function PropertyDetailInfoPage() {
             },
             {
               id: 'payments',
-              label: 'Payment Report',
+              label: 'Property Statement',
               icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z',
             },
             {
